6e4fd15cae011b11ac2f3a120ca4e2d01626d9ce
JohnnyBlomgren <johnny@gloot.com>
2021-07-22 21:44:00 +0200

    Added particle and emitter

---
+++ components/Particle.js
@@ -0,0 +1,42 @@
+import styled, { css, keyframes } from 'styled-components';
+
+const traverse = (x, y) => keyframes`
+  0% {
+    transform: translate(0,0);
+    opacity: 1;
+  }
+  100% {
+    transform: translate(${x}, ${y});
+    opacity: 0;
+  }
+`;
+
+const Wrapper = styled.div`
+  ${(props) => css`
+    display: inline-block;
+    position: absolute;
+    will-change: transform;
+    left: ${props.x};
+    top: ${props.y};
+    width: ${props.size};
+    height: ${props.size};
+    animation: ${traverse(props.dx, props.dy)} ${props.ttl} linear infinite;
+    background: ${props.color};
+    color: ${props.color};
+    border-radius: 50%;
+    box-shadow: 0px 0px 10px 0px;
+  `};
+`;
+
+export default function Particle({
+  color = '#fff',
+  size = '4px',
+  ttl = '0.5s',
+  x = 0,
+  y = 0,
+  dx = '250%',
+  dy = '250%',
+  onDeath = () => {},
+}) {
+  return <Wrapper color={color} size={size} ttl={ttl} x={x} y={y} dx={dx} dy={dy} onAnimationEnd={onDeath} />;
+}
---
+++ components/ParticleEmitter.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+
+import Particle from './Particle';
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+export default function ParticleEmitter({
+  count = 5,
+  sizes = ['4px', '6px'],
+  ttls = [5, 10],
+  colors = ['#fff', 'var(--pink-600)'],
+}) {
+  const getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  };
+
+  const [arr, setArr] = useState(
+    new Array(count).fill('').map((i) => ({
+      id: Math.random() * 1000,
+      size: sizes[Math.floor(Math.random() * sizes.length)],
+      color: colors[Math.floor(Math.random() * colors.length)],
+      ttl: `${getRandomIntInclusive(ttls[0], ttls[1])}s`,
+      x: `${getRandomIntInclusive(0, 100)}%`,
+      y: `${getRandomIntInclusive(0, 100)}%`,
+      dx: `${getRandomIntInclusive(-5000, 5000)}%`,
+      dy: `${getRandomIntInclusive(-5000, 5000)}%`,
+    }))
+  );
+
+  console.log(arr);
+
+  return (
+    <Wrapper>
+      {arr.map((particle) => (
+        <Particle {...particle} key={particle.id} />
+      ))}
+    </Wrapper>
+  );
+}
