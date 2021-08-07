16f073d578a211af2c1e8b46f4a5bfc674ea2d28
JohnnyBlomgren <johnny@gloot.com>
2021-08-07 14:29:13 +0200

    Added variant to RangeInput to not show knob (thumb) until hover

--- components/RangeInput.js
+++ components/RangeInput.js
@@ -1,22 +1,38 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export default styled.input.attrs({ type: 'range' })`
+export default styled.input.attrs((props) => {
+  const { min = 0, max = 100 } = props;
+  const target = max - min;
+  const percentage = (props.value / target) * 100;
+
+  return {
+    type: 'range',
+    style: {
+      background: `linear-gradient(to right, var(--pink-500) 0%, var(--pink-600) ${percentage}%, #fff ${percentage}%, #fff 100%)`,
+    },
+  };
+})`
   -webkit-appearance: none;
   -moz-appearance: none;
   outline: 0;
   height: 6px;
   border-radius: 9999px;
-  background: ${(props) =>
-    `linear-gradient(to right, var(--pink-500) 0%, var(--pink-600) ${props.value}%, #fff ${props.value}%, #fff 100%);`};
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.5);
 
   ::-webkit-slider-thumb {
+    ${(props) =>
+      props.knob === 'hover' &&
+      css`
+        opacity: 0;
+      `};
+
     -webkit-appearance: none;
     width: 18px;
     height: 18px;
     background-image: radial-gradient(circle, transparent, 0%, #f7f7fc 40%, var(--pink-600) 40%);
     border-radius: 50%;
     box-shadow: 0px 0px 4px 2px rgba(0, 0, 0, 0.5);
+    transition: opacity 0.075s var(--cubic);
   }
 
   ::-moz-range-thumb {
@@ -27,4 +43,14 @@ export default styled.input.attrs({ type: 'range' })`
     border-radius: 50%;
     box-shadow: 0px 0px 4px 2px rgba(0, 0, 0, 0.5);
   }
+
+  ${(props) =>
+    props.knob === 'hover' &&
+    css`
+      &:hover {
+        ::-webkit-slider-thumb {
+          opacity: 1;
+        }
+      }
+    `};
 `;
