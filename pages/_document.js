0407beba63a1e6a2c3c8090bea955930a1471043
JohnnyBlomgren <johnny@gloot.com>
2021-08-07 14:29:50 +0200

    Lato bold now added

--- pages/_document.js
+++ pages/_document.js
@@ -33,7 +33,7 @@ class MyDocument extends Document {
         <Head>
           <script async src="https://kit.fontawesome.com/dad3f36068.js" crossOrigin="anonymous"></script>
           <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
